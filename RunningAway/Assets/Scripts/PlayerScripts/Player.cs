d1bc2703c04cc35642526ffd04c27b0aa362c41b
StjeanPhil <philippe_spellerin@hotmail.ca>
2025-01-29 13:45:50 -0500

    startwalls

--- RunningAway/Assets/Scripts/PlayerScripts/Player.cs
+++ RunningAway/Assets/Scripts/PlayerScripts/Player.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed = 1;
+    [SerializeField] private float speed = 8;
     private float directionX = 0;
     private float directionY = 0;
+    [SerializeField] private static GameObject map;
+    //float mapHeight = map.scale.Y;
+    //float mapLength = map.scale.X;
+    [SerializeField] private Rigidbody2D body;
+
+    
+
+
+
     [SerializeField] private int health = 10;
     // Start is called before the first frame update
     void Start()
     {
-
-    }
+        //mapHeight = map.transform.scale.Y;
+        //mapLength = map.transform.scale.X;
+    }       
 
     // Update is called once per frame
     void Update()
     {
-        directionX = Input.GetAxis("Horizontal");
-        directionY = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(directionX, directionY, 0);
-
-        transform.Translate(direction*speed*Time.deltaTime);
-        Debug.Log(direction * speed * Time.deltaTime);
-        Debug.Log(health);
+        Movement();
 
     }
+    void Movement()
+    {
+              
+        directionX = Input.GetAxis("Horizontal");
+        directionY = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(directionX, directionY);
+
+    
+        body.velocity = direction * speed ;
+        if (this.transform.position[0] < 0)
+        {
+            this.transform.position[0] = 0;
+        }
+
+        
+
+
+
+
+
+
+
+
+    }   
 }
