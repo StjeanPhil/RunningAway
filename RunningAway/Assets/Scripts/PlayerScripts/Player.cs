d5338661fd96f6e5eb25ca42bf455ebd82789f02
StjeanPhil <philippe_spellerin@hotmail.ca>
2025-01-29 11:53:54 -0500

    Movement Start

--- RunningAway/Assets/Scripts/PlayerScripts/Player.cs
+++ RunningAway/Assets/Scripts/PlayerScripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed = 1;
+    private float directionX = 0;
+    private float directionY = 0;
+    [SerializeField] private int health = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        directionX = Input.GetAxis("Horizontal");
+        directionY = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(directionX, directionY, 0);
+
+        transform.Translate(direction*speed*Time.deltaTime);
+        Debug.Log(direction * speed * Time.deltaTime);
+        Debug.Log(health);
+
     }
 }
